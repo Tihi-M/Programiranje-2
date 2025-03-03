9c6a5f20d6c50a3cde70a1dd7b8d3bbde1c0869b
Tihi-M <148490644+Tihi-M@users.noreply.github.com>
2025-03-03 18:34:53 +0100

    zadaci 8-14 sa pdf

--- binarnoStablo5/main.cpp
+++ binarnoStablo5/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <math.h>
+#include <queue>
 typedef struct Node{
     Node* left;
     Node* right;
@@ -25,9 +26,9 @@ void dodaj(Node*& head, int val){
         prev = temp;
         if(val < temp->val)
             temp = temp->left;
-        else if (val > temp->val)
+        else
             temp = temp->right;
-        else return;
+        //else return;
     }
 
     if(val < prev->val)
@@ -98,6 +99,65 @@ int suma1(Node* head){
     else return suma1(head->left) + suma1(head->right);
 }
 
+int suma2(Node* head,int lvl){
+    if(head == nullptr)
+        return 0;
+    if(lvl == 0){
+        if(head->val % 2 == 0)
+            return head->val;
+        else return -1*head->val;
+    }
+    else return suma2(head->left,lvl-1) + suma2(head->right,lvl-1);
+}
+
+bool simetricnoStablo(Node* leftt,Node* rightt){
+    if(leftt == nullptr && rightt == nullptr)
+        return true;
+    if(leftt == nullptr && rightt != nullptr)
+        return false;
+    if(leftt != nullptr && rightt == nullptr)
+        return false;
+    if(leftt->val != rightt->val)
+        return false;
+    return simetricnoStablo(leftt->left,rightt->right) && simetricnoStablo(leftt->right,rightt->left);
+}
+
+bool balansiranoStablo(Node* head){
+    if(head == nullptr)
+        return true;
+    if(abs(visinaStabla(head->left) - visinaStabla(head->right)) > 1)
+            return false;
+    else return balansiranoStablo(head->left) && balansiranoStablo(head->right);
+}
+
+void inorderQueue(Node* head,std::queue<int>& red){
+    if(head== nullptr)
+        return;
+    inorderQueue(head->left,red);
+    red.push(head->val);
+    inorderQueue(head->right,red);
+}
+
+bool validnoStablo(Node* head){
+    std::queue<int> red;
+    inorderQueue(head,red);
+
+    if(red.empty())
+        return true;
+
+    int temp = red.front();
+    red.pop();
+    while(!red.empty()){
+        if(temp > red.front())
+            return false;
+
+        temp = red.front();
+        red.pop();
+    }
+    return true;
+
+}
+
 int main()
 {
     Node* head = nullptr;
@@ -114,9 +174,15 @@ int main()
     dodaj(head,38);
     dodaj(head,48);
 
-    //inOrder(head);
     stampaNivoe(head);
+
+    //inOrder(head);
+
     //std::cout<<prebroj(head)<<std::endl;
-    std::cout<<suma1(head);
+   // std::cout<<suma1(head);
+    //std::cout<<suma2(head,2);
+
+    //std::cout<<simetricnoStablo(head->left,head->right);
+    std::cout<<validnoStablo(head);
     return 0;
 }
